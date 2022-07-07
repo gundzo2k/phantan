f4a83b77189da2df3749daa0da611f03c9c62b65
Vietanh1102 <59017514+t01292243272@users.noreply.github.com>
2022-07-07 08:01:10 +0700

    Update login.php

--- app/Views/Client/login.php
+++ app/Views/Client/login.php
@@ -17,8 +17,10 @@
                                 <div class="form-group">
                                     <input class="form-control" placeholder="Mật khẩu" name="password" type="password" value="" id="password">
                                 </div>
-                                <!-- Change this to a button or input when using this as a form -->
                                 <button type="button" id="btn-login" class="btn btn-lg btn-success btn-block">Đăng nhập</button>
+                                <div class="form-group register">
+                                    <span>Bạn chưa có tài khoản ? <a href="./register">Đăng ký ngay</a></span>
+                                </div>
                             </fieldset>
                         </form>
                     </div>
