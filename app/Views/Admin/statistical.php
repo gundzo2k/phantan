6892624ccc6837a4fb1c372449d84e5c499125fa
Vietanh1102 <59017514+t01292243272@users.noreply.github.com>
2022-07-07 07:57:44 +0700

    Update VA

--- app/Views/Admin/statistical.php
+++ app/Views/Admin/statistical.php
@@ -1,81 +1,48 @@
 <?= $header ?>
-<div class="page-statistical">
-    <div class="statistical-box">
-        <div class="statistical-box-title">
-            <h3>Thống kê doanh thu</h3>
-        </div>
-        <div class="list-statistical">
-            <div class="item">
-                <div class="desc">
-                    <div class="amount"><?= number_format(array_sum(array_column($revenueToday, 'rentCost'))) ?> VNĐ</div>
-                    <div class="title">Doanh thu hôm nay</div>
-                </div>
-                <div class="icon"><i class="fa fa-bar-chart"></i></div>
-            </div>
-            <div class="item">
-                <div class="desc">
-                    <div class="amount"><?= number_format(array_sum(array_column($revenueMonth, 'rentCost'))) ?> VNĐ</div>
-                    <div class="title">Doanh thu tháng này</div>
-                </div>
-                <div class="icon"><i class="fa fa-bar-chart"></i></div>
-            </div>
-            <div class="item">
-                <div class="desc">
-                    <div class="amount"><?= count(array_column($revenueToday, 'rentCost')) ?></div>
-                    <div class="title">Phòng thuê hôm nay</div>
-                </div>
-                <div class="icon"><i class="fa fa-calendar-check-o"></i></div>
-            </div>
-            <div class="item">
-                <div class="desc">
-                    <div class="amount"><?= count(array_column($listRoom, 'id')) ?></div>
-                    <div class="title">Tổng số phòng học</div>
-                </div>
-                <div class="icon"><i class="fa fa-building-o"></i></div>
-            </div>
-            <div class="item">
-                <div class="desc">
-                    <div class="amount"><?= $getAmountUserJoinThisWeek ?></div>
-                    <div class="title">Thành viên mới tham gia</div>
-                </div>
-                <div class="icon"><i class="fa fa-user-plus"></i></div>
-            </div>
-            <div class="item">
-                <div class="desc">
-                    <div class="amount"><?= $getTotalUser ?></div>
-                    <div class="title">Tổng số thành viên</div>
-                </div>
-                <div class="icon"><i class="fa fa-group"></i></div>
-            </div>
-        </div>
-    </div>
 
-    <div class="room-statistics">
-        <div class="statistical-box">
-            <div class="statistical-box-title">
-                <h3>Thống kê phòng</h3>
+<div class="page-detail">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Thống kê doanh thu tháng này</h3>
+                </div>
             </div>
-            <div class="list-statistical">
-                <table class="table table-responsive">
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <table class="table table-striped">
                     <thead>
                         <tr>
+                            <th>Người thuê</th>
                             <th>Tên phòng</th>
-                            <th>Số ca thuê</th>
-                            <th>Tổng doanh thu</th>
+                            <th>Số người</th>
+                            <th>Thuê ngày</th>
+                            <th>Ca thuê</th>
+                            <th>Doanh thu</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <?php foreach ($getStatisticRoom as $room): ?>
+                        <?php if (!isset($rooms) || count($rooms) == 0): ?>
                             <tr>
-                                <td><?= $room['name'] ?></td>
-                                <td><?= $room['amount_shift'] ?></td>
-                                <td><?= number_format($room['revenue']) ?> VNĐ</td>
+                                <td colspan="6">Không có dữ liệu</td>
                             </tr>
-                        <?php endforeach; ?>
+                        <?php else: ?>
+                        <?php foreach ($rooms as $key => $value) { ?>
+                            <tr>
+                                <td><?= $value['name'] ?></td>
+                                <td><?= $value['room_name'] ?></td>
+                                <td><?= $value['size'] ?></td>
+                                <td><?= date('d/m/Y', strtotime($value['date_hire'])) ?></td>
+                                <td><?= $value['shift_id'] ?></td>
+                                <td><?= number_format($value['rentCost']) ?> VNĐ</td>
+                            </tr>
+                        <?php } 
+                        endif; ?>
                     </tbody>
                 </table>
-            </div>
         </div>
     </div>
 </div>
+
 <?= $footer ?>
